1068dada6c4a1abed19b7d045e9effeb10550506
Ваше имя английскими буквами <fieryswv@yandex.ru>
2023-07-20 10:07:45 +0300

    Init coomit

---
+++ Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите натуральное число для N: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 2;
+printNumber(n, count);
+Console.Write(1);
+
+void printNumber(int n, int count)
+{
+  if (count > n) return;
+  printNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число для M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите натуральное число для N: ");
+int n = int.Parse(Console.ReadLine()!);
+int temp = m;
+
+if (m > n)
+{
+  m = n;
+  n = temp;
+}
+
+findSum(m, n, temp = 0);
+
+void findSum(int m, int n, int summ)
+{
+  if (m > n)
+  {
+    m = n;
+    n = temp;
+  }
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {summ} ");
+    return;
+  }
+  findSum(m, n - 1, summ);
+}
